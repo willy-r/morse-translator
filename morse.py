8cfa12c9ad7ed88b4e9e79d15389f372e1cc9797
William Rodrigues <williamrodrigues2442@gmail.com>
2020-11-08 23:33:49 -0300

    added basic cli

--- morse.py
+++ morse.py
@@ -1,30 +1,36 @@
-import sys
-
-from utils import get_pattern
+from utils import get_pattern, get_args
 
 
-def to_morse(sentence: str) -> str:
-    morse_sentence = []
-    for chars in sentence.split():
+def to_morse(text: str) -> str:
+    """Translate normal text to Morse Code."""
+    morse_code = []
+    for chars in text.split():
         for char in set(chars):
             code = get_pattern(char, search_key='to')
             chars = chars.replace(char, f'{code} ')
-        morse_sentence.append(chars)
-    return '/ '.join(morse_sentence)
+        morse_code.append(chars)
+    return '/ '.join(morse_code)
 
 
-def from_morse(morse_sentence: str) -> str:
-    sentence = []
-    for morse_word in morse_sentence.split(' / '):
+def from_morse(morse_code: str) -> str:
+    """Translate Morse Code to normal text."""
+    text = []
+    for morse_word in morse_code.split(' / '):
         codes = morse_word.split()
         for i, code in enumerate(codes):
             char = get_pattern(code, search_key='from')
             codes[i] = char
-        sentence.append(''.join(codes))
-    return ' '.join(sentence).upper()
+        text.append(''.join(codes))
+    return ' '.join(text).upper()
+
+
+def main() -> None:
+    args = get_args()
+    if args._to is not None:
+        print(to_morse(args._to))
+    else:
+        print(from_morse(args._from))
 
 
 if __name__ == '__main__':
-    phrase = sys.argv[1]
-    print(to_morse(phrase))
-    print(from_morse(phrase))
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 from os.path import dirname, abspath
 
 filename = 'patterns.json'
@@ -16,5 +17,33 @@ You can download a copy of this file here:
 
 def get_pattern(pattern: str, *, search_key: str) -> str:
     if search_key not in ['to', 'from']:
-        raise KeyError('Not a valid key, enter ‘to’ or ‘from’')
+        raise KeyError('Not a valid key, enter "to" or "from"')
+
     return PATTERNS[search_key].get(pattern.lower(), '#')
+
+
+def get_args() -> argparse.Namespace:
+    p = argparse.ArgumentParser(prog='morse',
+                                description='Translate to and from Morse Code')
+    p.version = '1.0'
+    g = p.add_mutually_exclusive_group(required=True)
+
+    p.add_argument('-v',
+                   '--version',
+                   action='version')
+
+    g.add_argument('-t',
+                   '--to',
+                   action='store',
+                   metavar='TEXT',
+                   dest='_to',
+                   help='translate normal TEXT to morse')
+
+    g.add_argument('-f',
+                   '--from',
+                   action='store',
+                   metavar='MORSE',
+                   dest='_from',
+                   help=('translate MORSE (using "." or "-", separating '
+                         'letters by spaces and words by "/") to text'))
+    return p.parse_args()
